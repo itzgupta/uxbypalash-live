4e300ba5b68dd107cc73814a47cf5a0f912da153
itzgupta <itz.palashgupta@gmail.com>
2023-03-29 00:35:29 +0530

    responsive-update-2

--- src/components/Cases.jsx
+++ src/components/Cases.jsx
@@ -8,15 +8,15 @@ const Cases = () => {
     <div className='bg-white border-b-[1px] border-[#D4D4D4]'>
         <div className='p-4 max-w-[1240px] m-auto'>
             <div>
-                <h2 className='flex text-[40px] font-bold py-4'>Case Studies / Interfaces</h2>
+                <h2 className='flex text-[24px] sm:text-[40px] font-bold py-4'>Case Studies / Interfaces</h2>
                 <p className=' text-[18px] sm:text-[20px] font-medium text-[#5F5F5F]'>
                     Discover my product design projects that aim to enhance user experience through potential feature identification. <br />
                     Check out my <span className='text-[#FF477E] font-medium underline underline-offset-4'><Link href='https://dribbble.com/uxbypalash'>Dribbble</Link></span> and <span className='text-[#F99D23] font-medium underline underline-offset-4'><Link href='https://www.instagram.com/uxbypalash'>Instagram</Link></span> for my hobby explorations of color palettes and UI trends.
                 </p>
             </div>
 
-            <div className='mt-4'>
-                <div className='flex flex-nowrap gap-4'>
+            <div className='flex flex-col mt-4'>
+                <div className='sm:flex flex-nowrap gap-4'>
                     <div className='flex-col hover:animate-pulse'p-4>
                         <div className='py-1'>
                             <Link href='/'><img src="./assets/Debicover.svg" alt="" /></Link>
