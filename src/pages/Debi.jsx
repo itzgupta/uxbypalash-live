7e951c722203fcb5b1e56710bc1959c34b266f6e
itzgupta <itz.palashgupta@gmail.com>
2023-03-29 01:38:20 +0530

    debi-img-response

--- src/pages/Debi.jsx
+++ src/pages/Debi.jsx
@@ -6,9 +6,9 @@ const Debi = () => {
         <div>
             {/* ---- BANNER ---- */}
             
-            <div className='flex flex-col mt-10 items-center justify-center h-[590px] bg-[#EDF2FB] px-5'>
+            <div className='flex flex-col mt-10 items-center justify-center h-[450px] bg-[#EDF2FB] px-5'>
                 <p className='text-[32px] py-4 text-center font-bold'>Debi</p>
-                <img className='h-[40%] sm:h-[60%] ml-2' src="./assets/Debibanner.svg" alt="" />
+                <img className='h-[50%] sm:h-[60%] ml-2' src="./assets/Debibanner.svg" alt="" />
             </div>
 
             {/* ---- Overview ---- */}
