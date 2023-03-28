3b9b9bde1931a775e13cbd344964ab85e6ccf686
itzgupta <itz.palashgupta@gmail.com>
2023-03-29 01:05:09 +0530

    color-update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <h4 className='text-[32px] font-bold py-4'>Let's Connect.</h4>
             <p className='text-[16px] sm:text-[20px] font-medium text-[#5F5F5F]'>
                 Do you have a specific project or opportunity that you would like to discuss? <br />
-                Let's have a pleasant conversation about it.<span className='font-bold'> You can reach me by sending <br />
+                Let's have a pleasant conversation about it.<span className='font-bold text-[#FF0A54]'> You can reach me by sending <br />
                 an email to my address.</span>
             </p>
             <button className='text-white text-[20px] font-medium rounded border-2 bg-black py-[20px] px-[40px] my-8 hover:text-black hover:bg-white hover:border-black'>
