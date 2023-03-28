fb9f743f064e3148b72882e82d97cc49bcaa0bb8
itzgupta <itz.palashgupta@gmail.com>
2023-03-28 17:26:20 +0530

    case-update2

--- src/components/Cases.jsx
+++ src/components/Cases.jsx
@@ -4,18 +4,23 @@ import Link from 'next/link'
 
 const Cases = () => {
   return (
-    <div>
+    <div className=''>
         <div>
-            <h2>Case Studies / Interfaces</h2>
-            <p>
+            <h2 className='max-w-[1240px] m-auto flex text-[40px] font-bold p-4'>Case Studies / Interfaces</h2>
+            <p className='max-w-[1240px] m-auto flex-col p-4 text-[20px]'>
                 Discover my product design projects that aim to enhance user experience through potential feature identification. <br />
-                Check out my <span>Dribbble</span> and <span>Instagram</span> for my hobby explorations of color palettes and UI trends.
+                Check out my 
+                <Link href='https://dribbble.com/uxbypalash'><span className='text-[#FF477E] font-medium underline'> Dribbble</span></Link> and <Link href='https://www.instagram.com/uxbypalash'><span className='text-[#F99D23] font-medium underline'>Instagram</span></Link> for my hobby explorations of color palettes and UI trends.
             </p>
         </div>
 
         <div>
             <div>
-                <Link href='/'><img src="./assets/Debicover.svg" alt="" /></Link>
+                
+                <div>
+                    <Link href='/'><img src="./assets/Debicover.svg" alt="" /></Link>
+                </div>
+                
                 <h5>Debi - A Safe transaction Application</h5>
                 <p>
                     Debi is the faster, safer way to send and receive money or make <br />
