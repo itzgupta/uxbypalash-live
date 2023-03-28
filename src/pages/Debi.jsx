6bdfc955f81dde6e0210df8c403efd5d6e9053f3
itzgupta <itz.palashgupta@gmail.com>
2023-03-29 01:36:28 +0530

    debi-update-2

--- src/pages/Debi.jsx
+++ src/pages/Debi.jsx
@@ -4,7 +4,18 @@ const Debi = () => {
   return (
     <div className=''>
         <div>
+            {/* ---- BANNER ---- */}
             
+            <div className='flex flex-col mt-10 items-center justify-center h-[590px] bg-[#EDF2FB] px-5'>
+                <p className='text-[32px] py-4 text-center font-bold'>Debi</p>
+                <img className='h-[40%] sm:h-[60%] ml-2' src="./assets/Debibanner.svg" alt="" />
+            </div>
+
+            {/* ---- Overview ---- */}
+
+            <div>
+                
+            </div>
         </div>
     </div>
   )
