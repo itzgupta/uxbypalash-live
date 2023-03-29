031803e35293b066a9ac24360ed198862baa6f31
uxbypalash <itz.palashgupta@gmail.com>
2023-03-29 16:31:58 +0530

    Debi-update

--- src/pages/Debi.jsx
+++ src/pages/Debi.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
 const Debi = () => {
@@ -7,14 +8,18 @@ const Debi = () => {
             {/* ---- BANNER ---- */}
             
             <div className='flex flex-col mt-10 items-center justify-center h-[450px] bg-[#EDF2FB] px-5'>
-                <p className='text-[32px] py-4 text-center font-bold'>Debi</p>
-                <img className='h-[50%] sm:h-[60%] ml-2' src="./assets/Debibanner.svg" alt="" />
+              <p className='text-[32px] py-4 text-center font-bold'>Debi</p>
+              <img className='h-[50%] sm:h-[60%] ml-2' src="./assets/Debibanner.svg" alt="" />
             </div>
 
             {/* ---- Overview ---- */}
 
-            <div>
-                
+            <div className='p-8 max-w-[1240px] m-auto'>
+              <div>
+                <h5 className='text-[20px]'><span className='text-[#0116E9] font-bold'>Role :</span> UI/UX Designer</h5>
+                <h5 className='text-[20px]'><span className='text-[#0116E9] font-bold'>Project Type :</span> Personal Project</h5>
+                <h5 className='text-[20px]'><span className='text-[#0116E9] font-bold'>Duration :</span> 4 weeks</h5>
+              </div>
             </div>
         </div>
     </div>
